a8fae36ba06e219eb6fe10409836dc94f72b04c8
Mo Chahadeh <applednd2002@gmail.com>
2023-04-24 16:51:27 +0300

    shared_mem file

---
+++ shared_mem.c
@@ -0,0 +1,9 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
